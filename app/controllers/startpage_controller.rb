ec2660eb6ca3dba6abaadcbd8c919f02159f6b73
Markus Schwarz <post@markus-schwarz.net>
2016-03-14 20:23:58 +0100

    redirects to current team players if current team is selected

--- app/controllers/startpage_controller.rb
+++ app/controllers/startpage_controller.rb
@@ -1,5 +1,9 @@
 class StartpageController < ApplicationController
   def show
-     redirect_to players_path if can? :index, Player
+    if current_team
+      redirect_to team_players_path(current_team) if can? :index, Player
+    else
+      redirect_to players_path if can? :index, Player
+    end
   end
 end
